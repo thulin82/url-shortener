f46265dab18c65cd091f79b451e4220453028144
Markus Thulin <macky_b@hotmail.com>
2019-07-16 22:42:54 +0200

    Adding code for forwarding from short url to full url

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const Url = require('../models/Url');
+
+router.get('/:code', async (req, res)=> {
+    try {
+        const url = await Url.findOne({ urlCode: req.params.code});
+
+        if (url) {
+            return res.redirect(url.longUrl);
+        } else {
+            return res.status(404).json('No ur found');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json('Server error');
+    }
+});
+
 module.exports = router;
